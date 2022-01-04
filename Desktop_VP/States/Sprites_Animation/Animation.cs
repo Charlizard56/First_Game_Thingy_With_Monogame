b9db27efae64e2bbbf164834c2c4e0cf08926dcb
DM <inspirednorthstar@gmail.com>
2022-01-03 21:39:29 -0500

    Enhance Features

--- Desktop_VP/States/Sprites_Animation/Animation.cs
+++ Desktop_VP/States/Sprites_Animation/Animation.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
-
+using System.Diagnostics;
 
 namespace Desktop_VP
 {
@@ -8,7 +8,7 @@ namespace Desktop_VP
         private float Time;
         public float time { get { return Time; } set { Time = value; } }
         private int Frame;
-        private bool Loop, Back;
+        private bool Back;
 
         /// <summary>
         /// <para>Flips thorugh Rect array</para>
@@ -17,13 +17,22 @@ namespace Desktop_VP
         /// <param name="Time_R"></param>
         /// <returns>Frame</returns>
         public int Animate(Rectangle[] Source,float _Time_R,bool _Loop) {
+            try
+            {
+
+                Time--;
+                if (Time <= 0)
+                {
+                    An(Source, _Time_R, _Loop);
+                }
+                
+            }
             
-            Time--;
-            if (Time <= 0) {
-                An(Source, _Time_R, _Loop);
-            }          
+            catch {
+                Debug.WriteLine("Failed to Animate");
+            }
+
             return Frame;
-            
         }
 
         public void An(Rectangle[] Source,float Time_R,bool _Loop)
